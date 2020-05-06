53312c55b6a5a153dc6ec11e2e402e8619ef596d
xjc <2585441871@qq.com>
2020-05-06 20:34:45 +0800

    会员卡

--- src/pages/member/index.vue
+++ src/pages/member/index.vue
@@ -1,7 +1,17 @@
 <!-- 会员 -->
 <template>
   <div class="member">
-    会员
+    <div class="headerbar">
+      <div class="titleLayout clearfix">
+        <div class="rightIcon fr"></div>
+        <div class="title">开启创业</div>
+      </div>
+    </div>
+    <div class="vipcart"></div>
+    <div class="openNow">
+      立即开通
+    </div>
+
     <bottomBanner :index="2" />
   </div>
 </template>
@@ -32,7 +42,57 @@ export default {
 
 <style  lang="less">
 .member {
-  
+  & .headerbar {
+    width: 375px;
+    height: 200px;
+    background: #c61c1c;
+    & .titleLayout {
+      padding-top: 30px;
+      text-align: center;
+      & .title {
+        font-size: 18px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: rgba(255, 255, 255, 1);
+        line-height: 25px;
+      }
+      & .rightIcon {
+        width: 22px;
+        height: 22px;
+        background: rgba(255, 255, 255, 1);
+        margin-right: 13px;
+      }
+    }
+  }
+  & .vipcart {
+    margin: -20px 10px 0;
+    width: 355px;
+    height: 150px;
+    background: linear-gradient(
+      225deg,
+      rgba(191, 191, 191, 1) 0%,
+      rgba(115, 115, 115, 1) 100%
+    );
+    border-radius: 4px;
+  }
+  & .openNow {
+    width: 305px;
+    height: 40px;
+    line-height: 40px;
+    background: linear-gradient(
+      45deg,
+      rgba(237, 73, 73, 1) 0%,
+      rgba(198, 28, 28, 1) 100%
+    );
+    border-radius: 20px;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    margin: -20px auto 0;
+    text-align: center;
+    font-size:15px;
+    font-family:PingFang-SC-Bold,PingFang-SC;
+    font-weight:bold;
+    color:rgba(255,255,255,1);
+  }
 
   .fl {
     float: left;
